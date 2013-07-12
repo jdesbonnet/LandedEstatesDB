5bdb9a2b5df9001af906519b4c9022ea6b68138a
Joe Desbonnet <jdesbonnet@gmail.com>
2013-07-12 20:30:30 +0100

    no comment

--- WEB-INF/src/ie/wombat/landedestates/Family.java
+++ WEB-INF/src/ie/wombat/landedestates/Family.java
@@ -32,7 +32,7 @@ public class Family implements Indexable {
 	private String description;
 	
 	@ManyToMany
-	@JoinTable(name="estates_families",
+	@JoinTable(name="estate_families",
 	joinColumns=@JoinColumn(name="family_id"),
 	inverseJoinColumns = @JoinColumn(name="estate_id")
 	)
