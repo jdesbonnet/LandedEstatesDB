a6787c297d045f771ca308d172462550b8108c0c
Joe Desbonnet <jdesbonnet@gmail.com>
2018-01-03 00:40:57 +0000

    no comment

--- WEB-INF/src/ie/wombat/landedestates/AppContextStart.java
+++ WEB-INF/src/ie/wombat/landedestates/AppContextStart.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletContextListener;
 
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.apache.velocity.app.Velocity;
 
 /**
  * Responsible for initializing the app. contextInitialized() is called first 
@@ -29,6 +30,21 @@ public class AppContextStart implements ServletContextListener{
 		
 		File appRoot = new File(event.getServletContext().getRealPath("/"));
 		Configuration.init(appRoot);
+		
+		/*
+		 try
+		 {
+		     Velocity.setProperty(Velocity.ENCODING_DEFAULT,"UTF-8");
+		     Velocity.setProperty("runtime.log.logsystem.class", "org.apache.velocity.runtime.log.NullLogSystem");
+		     // Allow relative paths in VM include/parse directives
+		     Velocity.setProperty("eventhandler.include.class", "org.apache.velocity.app.event.implement.IncludeRelativePath");
+		     Velocity.init();
+		 }
+		 catch (Exception e)
+		 {
+		     e.printStackTrace();
+		 }
+		 */
 	}
 
 }
--- jsp/backend/family-query.jsp
+++ jsp/backend/family-query.jsp
@@ -1,15 +1,9 @@
 <%@include file="_header.jsp"%><%
-
 	String query = request.getParameter("query");
-	String hquery = "from Family where name like ? order by name";
-	
-	System.err.println ("Query="+hquery);
-	
-	Iterator iter = hsession.createQuery(hquery)
-							.setString(0,query + "%")
-							.list().iterator();
-	while (iter.hasNext()) {
-		Family family = (Family)iter.next();
+	List<Family> families = em.createQuery("from Family where name like :q order by name")
+							.setParameter("q",query + "%")
+							.getResultList();
+	for(Family family : families) {
 		out.write (family.getName());
 		out.write (" [");
 		out.write (family.getId().toString());
